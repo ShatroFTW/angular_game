23e3ded8b7d6e1827c9ab892964d71e168e6671b
Shatro <shatro@live.de>
2024-08-16 20:17:36 +0200

    working navbar dummy with bootstrap

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NgbNavModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
