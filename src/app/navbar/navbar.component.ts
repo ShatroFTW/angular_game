96e8df5b745e36cc8ff8a41a865334697a0a63b6
Shatro <shatro@live.de>
2024-08-16 20:28:04 +0200

    added inventory component and loaded via nav

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { InventoryComponent } from '../inventory/inventory.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgbNavModule],
+  imports: [NgbNavModule, InventoryComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
